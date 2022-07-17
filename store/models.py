6fa72f8f5523aebb53f91229663b640814a6ebe4
EuniceKiingati <47145776+EuniceKiingati@users.noreply.github.com>
2022-07-17 21:54:57 +0300

    added signal file

--- store/models.py
+++ store/models.py
@@ -10,14 +10,14 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    first_name=models.CharField(max_length=255, null=True)
-    last_name=models.CharField(max_length=255)
-    email=models.EmailField(unique=True)
-    phone=models.CharField(max_length=255)
+    first_name=models.CharField(max_length=255, null=True,blank=True)
+    last_name=models.CharField(max_length=255, null=True,blank=True)
+    email=models.EmailField(unique=True , null=True,blank=True)
+    phone=models.CharField(max_length=255 , null=True,blank=True)
     
     
     def __str__(self):
-        return self.first_name
+        return self.user.username
 
 class Product(models.Model):
     name=models.CharField(max_length=255)
